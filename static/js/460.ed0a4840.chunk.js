1b6739749933f4eb484e748d3d37cb51574ef111
Jain, Kalpeshkumar D <kalpeshkumar.jain@sscinc.com>
2023-12-11 15:52:45 +0530

    Updates

---
+++ static/js/460.ed0a4840.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkShubham_Computers=self.webpackChunkShubham_Computers||[]).push([[460],{3589:function(e,a,r){r.r(a),r.d(a,{default:function(){return h}});var s=r(2791),u=r.p+"static/media/hero-image.6a1c956adafe5fe78a23.jpg",c=r(7087),n=r(184),t=function(){return(0,n.jsxs)("div",{className:"hero-wrapper",children:[(0,n.jsx)(c.LazyLoadImage,{src:u,alt:"",effect:"blur"}),(0,n.jsx)("h1",{children:"SHUBHAM COMPUTERS"}),(0,n.jsx)("p",{children:"COMPUTER ACCESSORIES and more..."})]})},h=s.memo(t)}}]);
+//# sourceMappingURL=460.ed0a4840.chunk.js.map
